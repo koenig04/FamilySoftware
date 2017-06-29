8d9c49c0e36402fad9229b6bbb851f9d13378660
lukuan <lukuan04@hotmail.com>
2017-06-29 13:58:44 +0800

    commit missing files

---
+++ FamilyAsset/FamilyAsset/Pages/UIControlNames.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyAsset.Pages
+{
+    class UIControlNames
+    {
+        public const string SysConfigure = "SysConfigure";
+        public const string ItemConfigure = "ItemConfigure";
+    }
+}
---
+++ FamilyAsset/FamilyAsset/UserControlViewModelBase.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using FamilyAsset.UICore;
+
+namespace FamilyAsset
+{
+    abstract class UserControlViewModelBase : NotificationObject, IUserControlViewModel
+    {
+        public event EventHandler<UserControlMessageEventArgs> UserControlMessageEvent;
+
+        private Visibility _vis = Visibility.Visible;
+
+        public Visibility Vis
+        {
+            get { return _vis; }
+            set
+            {
+                _vis = value;
+                RaisePropertyChanged("Vis");
+            }
+        }
+
+        public abstract void HandleViewModelCallBack(ViewModelCallBackInfo callbackInfo);
+        public void HandleVisablilityControl(bool visibility)
+        {
+            this.Vis = visibility ? Visibility.Visible : Visibility.Hidden;
+        }
+        protected void RaiseUserControlMessageEvent(UserControlMessageEventArgs e)
+        {
+            if (UserControlMessageEvent != null)
+            {
+                UserControlMessageEvent(null, e);
+            }
+        }
+    }
+}
