d6bf7d6f63c64dd00bef9fb8b56deb4359b20463
lukuan <lukuan04@hotmail.com>
2017-10-05 20:44:52 +0800

    modify statistic UI

--- FamilyAsset/FamilyAsset/Pages/Statistic/AccountDetail/TimeDetail/AccountTimeCollectionViewModel.cs
+++ FamilyAsset/FamilyAsset/Pages/Statistic/AccountDetail/TimeDetail/AccountTimeCollectionViewModel.cs
@@ -1,12 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using BLL.StatisticProcess.DiagramRelative;
+using Common;
+using FamilyAsset.UICore;
 
 namespace FamilyAsset.Pages.Statistic.AccountDetail
 {
-    class AccountTimeCollectionViewModel
+    class AccountTimeCollectionViewModel : NotificationObject
     {
+        private Visibility _vis;
+
+        public Visibility TimeVis
+        {
+            get { return _vis; }
+            set
+            {
+                _vis = value;
+                RaisePropertyChanged("TimeVis");
+            }
+        }
+
+        private ObservableCollection<AccountTimeDetailViewModel> _timeCollection;
+
+        public ObservableCollection<AccountTimeDetailViewModel> TimeCollection
+        {
+            get { return _timeCollection; }
+            set
+            {
+                _timeCollection = value;
+                RaisePropertyChanged("TimeCollection");
+            }
+        }
+
+        public event EventHandler<StringEventArgs> ItemClickedEvent;
+
+        private void OnItemClickedEvent(object sender, StringEventArgs e)
+        {
+            if (ItemClickedEvent != null)
+            {
+                ItemClickedEvent(null, e);
+            }
+        }
+
+        public AccountTimeCollectionViewModel()
+        {
+            TimeCollection = new ObservableCollection<AccountTimeDetailViewModel>();
+        }
+
+        public void UpdateCurveData(BLL.StatisticProcess.DiagramRelative.CurveData detail)
+        {
+            TimeCollection.Clear();
+            foreach (AccountDetailByDate item in detail.Details)
+            {
+                TimeCollection.Add(new AccountTimeDetailViewModel(item));                
+            }
+        }
     }
 }
